835a0b79dab4a9348323f4eb8f256ac176b97b0a
Ainil Sepulveda <AinilSepulveda@gmail.com>
2020-11-28 15:03:13 -0300

    Revert "Revert "Merge branch 'Prueba' into main""
    This reverts commit ab7c801dd824218142f6e4fb49533923f8538005.

--- Assets/Scripts/Combat/MonoBehaviors/DestructedDestroyObject.cs
+++ Assets/Scripts/Combat/MonoBehaviors/DestructedDestroyObject.cs
@@ -7,6 +7,7 @@ public class DestructedDestroyObject : MonoBehaviour, IDestructible
 
     public void OnDestruction(GameObject destroyer)
     {
+        Debug.Log("Dead Idestructible");
         Destroy(gameObject);
     }
 }
